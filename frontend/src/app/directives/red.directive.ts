ee6bf8c6baf98b0b16a6982d5b39547d0d3b14a8
alinecbsr <alinecbsr@gmail.com>
2021-06-15 11:02:43 +0100

    Essential Angular Concepts, Attribute and Structural Directives in Practice

---
+++ frontend/src/app/directives/red.directive.ts
@@ -0,0 +1,11 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[styleRed]'
+})
+export class RedDirective {
+
+  constructor(private el: ElementRef) {
+    el.nativeElement.style.color = '#f50337'
+  }
+}
